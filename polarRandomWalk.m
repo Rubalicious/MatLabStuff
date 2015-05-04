087700d68be69953d881f7fc7ccac6f763077fa0
jtcelayaalcala <jtcelayaalcala@users.noreply.github.com>
2015-05-03 21:01:30 -0700

    Update polarRandomWalk.m

--- polarRandomWalk.m
+++ polarRandomWalk.m
@@ -1,12 +1,17 @@
 numIterations = 10000;
 x = 0;
 y = 0;
-figure;
+figure
+plot(x,y);
+
+disp('Press ^C to stop animation');
+
 while(numIterations>0)
     randomDirection = rand()*2*pi;
     x1 = 1*cos(randomDirection);
     y1 = 1*sin(randomDirection);
     line([x,x + x1], [y, y + y1]);
+    pause(0.00001);
     x = x + x1;
     y = y + y1;
     numIterations = numIterations -1;
