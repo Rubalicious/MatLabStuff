47bfad326cedb22293a5695f6620852db60c8517
jtcelayaalcala <jtcelayaalcala@users.noreply.github.com>
2015-05-03 20:45:31 -0700

    Update polarRandomWalk.m

--- polarRandomWalk.m
+++ polarRandomWalk.m
@@ -1,15 +1,13 @@
-numIterations = 1000;
-x = 1;
-y = 1;
+numIterations = 10000;
+x = 0;
+y = 0;
 figure;
 while(numIterations>0)
     randomDirection = rand()*2*pi;
-    randomLength = randomInt(2);
-    x1 = randomLength*cos(randomDirection);
-    y1 = randomLength*sin(randomDirection);
-    %r = sqrt(x1*x1 +y1*y1);
-    line([x, x1], [y, y1]);
-    x = x1;
-    y = y1;
+    x1 = 1*cos(randomDirection);
+    y1 = 1*sin(randomDirection);
+    line([x,x + x1], [y, y + y1]);
+    x = x + x1;
+    y = y + y1;
     numIterations = numIterations -1;
 end
